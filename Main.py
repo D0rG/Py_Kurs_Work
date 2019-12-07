4ad8ded2b6e8fdbc935882997b7dbba0a16d3eaa
Mixail <mixail.korchikov@mail.ru>
2019-12-07 19:37:13 +0300

    Больше функций и немного описания

--- Main.py
+++ Main.py
@@ -49,33 +49,57 @@ from Parking import Parking
 
 
 class Mywin(QtWidgets.QMainWindow):
-    def __init__(self):
+    ParkingsList = []
+    def __init__(self):  # Инициирует каждую кнопку
         super(Mywin, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("Py_Kurs_Work")
         self.ui.PrintDB.clicked.connect(self.DrawOnTimeTable)
         self.ui.Table.setColumnCount(4)  # Кол-во столбов
+        self.ui.BTCreatePark.clicked.connect(self.AddParking)
 
+    def AddParking(self):  # Добавляет парковку в лист парковок
+        ParkName = self.ui.TbParkName.text()
+        MaxPlaceDef = self.ui.TbMaxDef.text()
+        MaxPlaceVIP = self.ui.TbMaxVIP.text()
+        self.ui.TbMaxVIP.setText(None)
+        self.ui.TbMaxDef.setText(None)
+        self.ui.TbParkName.setText(None)
+        try:
+            self.ParkingsList.append(Parking(ParkName, int(MaxPlaceDef), int(MaxPlaceVIP)))
+            self.UpdateParkComboBox()
+        except Exception as e:
+            print(e)
 
-    def DrawOnTimeTable(self):
+    def UpdateParkComboBox(self):  # Обнавляет все комбобоксы с парковками
+        self.ui.SelectDelPark.clear()
+        self.ui.SelectPark.clear()
+        self.ui.SelectParkLeave.clear()
+
+        for park in self.ParkingsList:
+            self.ui.SelectDelPark.addItem(park.Name)
+            self.ui.SelectPark.addItem(park.Name)
+            self.ui.SelectParkLeave.addItem(park.Name)
+
+    def DrawOnTimeTable(self):  # Рисует выбранную пользователем БД
         try:
             buf = self.ui.SelectDB.currentIndex()
             if(buf == 0):
                 self.ui.Table.setHorizontalHeaderLabels(("ID", "Номер", "Время вьезда", "Время выезда"))  # Имена столбов у таблицы
-                data = ReturnTeble("CarParkingInfo")
-                row = 0
+                data = ReturnTeble("CarParkingInfo")  # Имя таблицы из которой будет рисоваться таблица
+                row = 0  # Строка
                 self.ui.Table.setRowCount(len(data))  # Кол-во строк
                 self.ui.Table.setColumnCount(4)  # Кол-во столбов
-                for tup in data:
-                    col = 0
-
-                    for item in tup:
-                        cellinfo = QTableWidgetItem(str(item))
+                for tup in data:  # Строка не поделённая на столбы
+                    col = 0  # Столбец
+                    for item in tup:  # Значение каждой клетки в одной строке (Разных стобах)
+                        cellinfo = QTableWidgetItem(str(item))  # Задаёт элемент
                         cellinfo.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)  # Только для чтения
-                        self.ui.Table.setItem(row, col, cellinfo)
+                        self.ui.Table.setItem(row, col, cellinfo)  # Вставляет элемент в определённую ячейку по строке/столбу
                         col += 1
                     row += 1
+
             elif(buf == 1):
                 data = ReturnTeble("Places")
                 row = 0
@@ -91,6 +115,7 @@ class Mywin(QtWidgets.QMainWindow):
                         self.ui.Table.setItem(row, col, cellinfo)
                         col += 1
                     row += 1
+
             elif(buf == 2):
                 data = ReturnTeble("VIP_List")
                 row = 0
@@ -105,6 +130,7 @@ class Mywin(QtWidgets.QMainWindow):
                         self.ui.Table.setItem(row, col, cellinfo)
                         col += 1
                     row += 1
+
         except Exception as e:
             self.ui.Table.setColumnCount(2)
             self.ui.Table.setRowCount(1)
